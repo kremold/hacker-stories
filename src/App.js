31e960572979285594cbfb74244c579d29752a04
Kyle R <pacman611@hotmail.com>
2021-03-10 16:09:57 -0600

    change readme and move map to own function

--- src/App.js
+++ src/App.js
@@ -27,11 +27,26 @@ function App() {
       <label htmlFor="search">Search:</label>
       <input id="search" type="text" />
 
-      {list.map((item) => {
-        return <div>{item.title}</div>;
-      })}
+      <hr />
+
+      <List />
     </div>
   );
+}
+
+function List() {
+  return list.map((item) => {
+    return (
+      <div key={item.objectID}>
+        <span>
+          <a href={item.url}>{item.title}</a>
+        </span>
+        <span>{item.author}</span>
+        <span>{item.num_comments}</span>
+        <span>{item.points}</span>
+      </div>
+    );
+  });
 }
 
 export default App;
