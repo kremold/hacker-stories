3034370da172cea988e84ce461d57e3d41b2f859
Kyle R <pacman611@hotmail.com>
2021-03-15 14:39:27 -0500

    lift searchTerm state to App, filter on titles

--- src/App.js
+++ src/App.js
@@ -21,9 +21,15 @@ const App = () => {
     },
   ];
 
+  const [searchTerm, setSearchTerm] = React.useState("");
+
   const handleSearch = (event) => {
-    console.log(event.target.value);
+    setSearchTerm(event.target.value);
   };
+
+  const searchedStories = stories.filter((story) => {
+    story.title.toLowerCase().includes(searchTerm.toLowerCase());
+  });
 
   return (
     <div>
@@ -33,28 +39,24 @@ const App = () => {
 
       <hr />
 
-      <List list={stories} />
+      <List list={searchedStories} />
     </div>
   );
 };
 
 const Search = (props) => {
-  const [searchTerm, setSearchTerm] = React.useState("");
-
   const handleChange = (event) => {
-    setSearchTerm(event.target.value);
-
     props.onSearch(event);
   };
 
   return (
     <div>
       <label htmlFor="search">Search:</label>
-      <input id="search" type="text" onChange={handleChange} />
+      <input id="search" type="text" onChange={props.onSearch} />
 
-      <p>
+      {/* <p>
         Searching for <strong>{searchTerm}</strong>.
-      </p>
+      </p> */}
     </div>
   );
 };
