11b234b642e52764cb1f0a2108e392c95bf2feef
Kyle R <pacman611@hotmail.com>
2021-03-19 10:49:45 -0500

    Fetch api data- imp hn api

--- src/App.js
+++ src/App.js
@@ -1,24 +1,7 @@
 import React from "react";
 import "./App.css";
 
-const initialStories = [
-  {
-    title: "React",
-    url: "https://reactjs.org/",
-    author: "Jordan Walke",
-    num_comments: 3,
-    points: 4,
-    objectID: 0,
-  },
-  {
-    title: "Redux",
-    url: "https://redux.js.org/",
-    author: "Dan Abramov, Andrew Clark",
-    num_comments: 2,
-    points: 5,
-    objectID: 1,
-  },
-];
+const API_ENDPOINT = "https://hn.algolia.com/api/v1/search?query=";
 
 const storiesReducer = (state, action) => {
   switch (action.type) {
@@ -53,14 +36,6 @@ const storiesReducer = (state, action) => {
   }
 };
 
-const getAsyncStories = () =>
-  new Promise((resolve) =>
-    setTimeout(() => resolve({ data: { stories: initialStories } }), 2000)
-  );
-//fake an error
-// const getAsyncStories = () =>
-//   new Promise((resolve, reject) => setTimeout(reject, 2000));
-
 const useSemiPersistentState = (key, initialState) => {
   // use the key so that 'value' in local storage isn't overwritten
   const [value, setValue] = React.useState(
@@ -86,11 +61,12 @@ const App = () => {
   React.useEffect(() => {
     dispatchStories({ type: "STORIES_FETCH_INIT" });
 
-    getAsyncStories()
+    fetch(`${API_ENDPOINT}react`)
+      .then((response) => response.json())
       .then((result) => {
         dispatchStories({
           type: "STORIES_FETCH_SUCCESS",
-          payload: result.data.stories,
+          payload: result.hits,
         });
       })
       .catch(() => dispatchStories({ type: "STORIES_FETCH_FAILURE" }));
